c426ee976f8d110d5a3800724a428268190f4072
borshon420 <borshonarafat420@gmail.com>
2021-12-26 21:30:55 +0600

    initial setup and create two blank page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+const Home = lazy(() => import('./components/Home')) ;
+const Details = lazy(() => import('./components/Details')) ;
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<p>Loading...</p>}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/details" component={Details} />
+        </Switch>
+      </Router>
+      </Suspense>
     </div>
   );
 }
